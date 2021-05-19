bfac0f01dc6cd6065965d941cbe6a3c2c5ffd37d
GianniCampana <campanagianni97@gmail.com>
2021-05-19 22:12:04 +0200

    title

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+<Title />
+
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Title
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'title'
+
+}
+</script>
+
+<style>
+
+</style>
